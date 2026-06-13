827b7ec9295a03e6adec627feb20ff3719ea2fdb
guenole das <guenoledas@proton.me>
2026-06-13 11:49:30 +0000

    POO

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,43 @@
 public class Main {
-    public static void main(String[] args) {
-        //System.out.println("hello word");
-        String example = "hello";
+    public static void main(String[] args){
+        Marque apple = new Marque("apple");
+        Incassable rinoshield = new Incassable("Rinoshield", true);
 
-        example = example.toUpperCase();
-        example = example.replace("HELL" , "yehh");
+        Telephone iphone =new Telephone(34000, "iphone", apple, rinoshield);
+        System.out.println(rinoshield.nom);
 
-        System.out.println(example);
+    }
+}
+
+
+class Telephone{
+    int pixels;
+    String nom;
+    Marque marque;
+    Incassable incassable;
+
+    public Telephone(int pixels, String nom, Marque marque, Incassable incassable) {
+        this.pixels = pixels;
+        this.nom = nom;
+        this.marque = marque;
+        this.incassable = incassable;
+    }
+}
+
+class Marque{
+    String nom;
+
+    public Marque(String nom) {
+        this.nom = nom;
+    }
+}
+
+class Incassable{
+    String nom;
+    boolean verre_tramper;
+
+    public Incassable(String nom, boolean verre_tramper) {
+        this.nom = nom;
+        this.verre_tramper = verre_tramper;
     }
 }
